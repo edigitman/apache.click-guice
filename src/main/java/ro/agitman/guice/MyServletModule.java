f6695e9912dd61bfc51587f8d3c15c86aa4592b2
Alex <edi.gitman@gmail.com>
2014-03-04 17:55:51 +0200

    Formated code, added DAO example

--- src/main/java/ro/agitman/guice/MyServletModule.java
+++ src/main/java/ro/agitman/guice/MyServletModule.java
@@ -1,5 +1,7 @@
 package ro.agitman.guice;
 
+import ro.agitman.model.dao.UserDao;
+import ro.agitman.model.dao.impl.UserDaoImpl;
 import ro.agitman.service.UserService;
 import ro.agitman.service.impl.UserServiceImpl;
 
@@ -10,8 +12,15 @@ public class MyServletModule extends ServletModule {
 	@Override
 	protected void configureServlets() {
 		super.configureServlets();
+
+		// mapped custom click servlet
 		serve("*.htm").with(MyClickServlet.class);
+
+		// mapped services
 		bind(UserService.class).to(UserServiceImpl.class);
+
+		// mapped dao
+		bind(UserDao.class).to(UserDaoImpl.class);
 	}
 
 }
