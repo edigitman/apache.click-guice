d6814d15473e2542c4bdd32116414043c2072311
Alex <edi.gitman@gmail.com>
2014-03-05 12:24:40 +0200

    Added shiro config

--- src/main/java/ro/agitman/guice/MyGuiceServletConfig.java
+++ src/main/java/ro/agitman/guice/MyGuiceServletConfig.java
@@ -1,5 +1,8 @@
 package ro.agitman.guice;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
@@ -10,7 +13,15 @@ import com.google.inject.servlet.GuiceServletContextListener;
  */
 public class MyGuiceServletConfig extends GuiceServletContextListener {
 
+	private ServletContext context;
+
+	@Override
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+		context = servletContextEvent.getServletContext();
+		super.contextInitialized(servletContextEvent);
+	}
+
 	protected Injector getInjector() {
-		return Guice.createInjector(new MyServletModule());
+		return Guice.createInjector(new MyServletModule(), new MyShiroWebModule(context));
 	}
 }
---
+++ src/main/java/ro/agitman/guice/MyShiroWebModule.java
@@ -0,0 +1,35 @@
+package ro.agitman.guice;
+
+import javax.servlet.ServletContext;
+
+import org.apache.shiro.config.Ini;
+import org.apache.shiro.guice.web.ShiroWebModule;
+import org.apache.shiro.realm.text.IniRealm;
+
+import com.google.inject.Provides;
+
+public class MyShiroWebModule extends ShiroWebModule {
+
+	public MyShiroWebModule(ServletContext servletContext) {
+		super(servletContext);
+	}
+
+	@Override
+	protected void configureShiroWeb() {
+		try {
+			bindRealm().toConstructor(IniRealm.class.getConstructor(Ini.class));
+		} catch (NoSuchMethodException e) {
+			addError(e);
+		}
+
+		addFilterChain("/public/**", ANON);
+		addFilterChain("/stuff/allowed/**", AUTHC_BASIC, config(PERMS, "yes"));
+		addFilterChain("/stuff/forbidden/**", AUTHC_BASIC, config(PERMS, "no"));
+		addFilterChain("/**", AUTHC_BASIC);
+	}
+
+	@Provides
+	Ini loadShiroIni() {
+		return Ini.fromResourcePath("classpath:shiro.ini");
+	}
+}
