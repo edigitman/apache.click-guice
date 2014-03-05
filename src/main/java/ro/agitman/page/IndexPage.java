cde34ad2ed5d22dc776fc27408f5855f28cb3d9d
Alex <edi.gitman@gmail.com>
2014-03-05 12:27:21 +0200

    Added index page

---
+++ src/main/java/ro/agitman/page/IndexPage.java
@@ -0,0 +1,12 @@
+package ro.agitman.page;
+
+import org.apache.click.Page;
+
+public class IndexPage extends Page {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3380751009541130928L;
+
+}
