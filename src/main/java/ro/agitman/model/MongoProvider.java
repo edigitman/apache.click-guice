730c05cbcb959e46c716636ab855adc46c51905a
Alex <edi.gitman@gmail.com>
2014-03-05 12:25:29 +0200

    Switch to morphia

--- src/main/java/ro/agitman/model/MongoProvider.java
+++ src/main/java/ro/agitman/model/MongoProvider.java
@@ -2,30 +2,32 @@ package ro.agitman.model;
 
 import java.net.UnknownHostException;
 
-import org.jongo.Jongo;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
 
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
-import com.mongodb.DB;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 
 @Singleton
-public class MongoProvider implements Provider<Jongo> {
+public class MongoProvider implements Provider<Datastore> {
 
-	private Mongo mongo;
-	private Jongo jongo;
+	private Mongo mongo = null;
+	private Morphia morphia = null;
+	private Datastore ds = null;
 
-	public Jongo get() {
-		if (mongo == null || jongo == null) {
+	public Datastore get() {
+		if (mongo == null || morphia == null) {
 			try {
 				mongo = new MongoClient("localhost", 27017);
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
 			}
-			DB db = mongo.getDB("dbname");
-			jongo = new Jongo(db);
+			morphia = new Morphia();
+			morphia.mapPackage("ro.agitman.model.mo");
+			ds = morphia.createDatastore(mongo, "social");
 		}
-		return jongo;
+		return ds;
 	}
 }
